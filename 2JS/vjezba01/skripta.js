6d5c5cda2ea17d5fdd24c52a2a0d57b04b654fcf
Boris-Despotovic <mrtecktonikboky@gmail.com>
2026-01-27 20:57:14 +0100

    JS vjezba01

---
+++ 2JS/vjezba01/skripta.js
@@ -0,0 +1,21 @@
+console.info('Hello world 6');
+console.table({ime: "Pero",prezime:"Perić"});
+
+document.getElementById('ispis').innerText='Hello world 7';
+document.getElementById('ispis').style.color='red';
+
+document.getElementById('ue').innerHTML=
+'<a href="#">Hello world 8</a>';
+console.log('Osnovna škola "Ivana Filipiovića"');
+
+console.log('Mc\'Donalds'); //ALTGR+Q\
+console.log("Mc'Donalds");
+
+// što ako želim u konzolu ispisati \
+console.log('onda radim \\ ovo');
+
+console.log(`
+    ovo je moj
+    tekst kroz
+    više redova
+`);
